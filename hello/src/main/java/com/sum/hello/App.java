2a090082d127e87105b2e5532c3794376f5ef9a4
SumalathaDesireddy <sumalathadesireddy@gmail.com>
2024-11-16 15:34:49 +0530

    adding package

---
+++ hello/src/main/java/com/sum/hello/App.java
@@ -0,0 +1,13 @@
+package com.sum.hello;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
